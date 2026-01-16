527932196fad39c9d5d0fc24abf9c5f17b2ac879
Jhlee <ljh990627@gmail.com>
2026-01-16 13:37:31 +0900

    플레이어/배경 움직임 구현
    1. 배경은 머터리얼의 오프셋을 변경하는 방식
    2. 캐릭터는 GetAxisRaw를 사용하여 가속도 삭제

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float scrollSpeed = 0.25f;
+    private Material myMaterial;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 오프셋을 머터리얼에서 가져온 후 offset 변경
+        Vector2 newOffset = myMaterial.mainTextureOffset;
+        newOffset.Set(0, newOffset.y + scrollSpeed * Time.deltaTime); // 프레임 보정 필수
+        myMaterial.mainTextureOffset = newOffset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 방향 * 시간 * 스피드
+        float distanceX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+        transform.Translate(distanceX, 0, 0);
+    }
+}
