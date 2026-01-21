5d3f398d4f9439279af482de7635c659a35cfe75
Jhlee <ljh990627@gmail.com>
2026-01-21 14:03:29 +0900

    코루틴 단순하게 테스트

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -9,6 +11,10 @@ public class GameManager : MonoBehaviour
     // 스코어
     int _score = 0;
     public Text scoreText;
+
+    // 스타트
+    public Text startText;
+    Coroutine startCoroutine;
 
     void Awake()
     {
@@ -22,11 +28,46 @@ public class GameManager : MonoBehaviour
 
         // 파괴 방지
         DontDestroyOnLoad(gameObject);
+
+        // 스타트 텍스트
+        startCoroutine = StartCoroutine(BlinkText(0.5f));
+        StartCoroutine(Countdown(3));
     }
 
+    // 스코어 추가
     public void Addscore(int amount)
     {
         _score += amount;
         scoreText.text = $"Score : {_score}";
+
+        // 난이도 변경
+        if (_score > 1000) SceneManager.LoadScene(1);
+    }
+
+    // 텍스트 깜빡임
+    IEnumerator BlinkText(float interval)
+    {
+        while (true)
+        {
+            if (startText != null) startText.enabled = !startText.enabled;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    // 카운트다운 표시
+    IEnumerator Countdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            startText.text = $"CountDown : {i}";
+            yield return new WaitForSeconds(1f);
+        }
+
+        startText.text = "Go!";
+
+        yield return new WaitForSeconds(1f);
+
+        StopCoroutine(startCoroutine);
+        startText.enabled = false;
     }
 }
